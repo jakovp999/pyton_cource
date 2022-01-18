3d7341588a31f911c12af3141a6592abbb454243
jakovp999 <jakovp999@gmail.com>
2022-01-18 19:40:24 +1100

    урок 1 задание 4

---
+++ HW_Lesson_1/les_1_ex_4.py
@@ -0,0 +1,16 @@
+chislo = int(input('Введите число'))
+
+chislo_max = chislo % 10
+# print(chislo_max)
+chislo = chislo // 10
+# print(chislo)
+
+while chislo > 0:
+    if chislo_max < chislo % 10:
+        chislo_max = chislo % 10
+    chislo = chislo // 10
+
+
+print('Максимально число')
+print(chislo_max)
+
