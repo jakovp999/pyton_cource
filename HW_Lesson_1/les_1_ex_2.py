9b17b8a27dc5a096662c48fe5e413aa9b763c041
jakovp999 <jakovp999@gmail.com>
2022-01-18 11:51:15 +1100

    второе задание 1 урока

---
+++ HW_Lesson_1/les_1_ex_2.py
@@ -0,0 +1,23 @@
+time = input('ВВедите вреря в секундах:  ')
+print(time)
+# print(type(time))
+
+time_hour = int(time) // 3600
+# print(time_hour)
+
+time_min = int(time) % 3600 // 60
+# print(time_min)
+time_sec = int(time) % 3600 % 60
+# print(time_sec)
+
+# Проверка
+
+# time_ver = time_hour * 3600 + time_min * 60 + time_sec
+# print(time_ver)
+# if time_ver!=time:
+#     print('Вычесленно верно')
+# else:
+#     print('Ошибка')
+
+result = f'{time_hour}:{time_min:02}:{time_sec:02}'
+print(result)
