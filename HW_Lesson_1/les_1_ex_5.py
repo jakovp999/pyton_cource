149a6ed777da2512cae42e9e8c6e9ef061e675bc
jakovp999 <jakovp999@gmail.com>
2022-01-19 04:34:28 +1100

    урок 1 задание 5, 6

--- HW_Lesson_1/les_1_ex_5.py
+++ HW_Lesson_1/les_1_ex_5.py
@@ -1,5 +1,6 @@
 viruchka = int(input('Введите выручку предприятия:  '))
 zatraty = int(input('Введите затраты предприятия:   '))
+kolv_sotr = int(input('Введите колличество сотрудников:   '))
 
 if viruchka > zatraty:
     result = viruchka - zatraty
@@ -8,5 +9,10 @@ elif viruchka < zatraty:
     result = zatraty - viruchka
     print(f'Предприятие работает с убытком {result}')
 else:
+    result = 0
     print('Предприятие работает с нулевой рентабельностью')
 
+rentabilnost = result / viruchka
+print(f'Рентабельность составляет {rentabilnost}')
+prib_for_one = result / kolv_sotr
+print(f'Прибыль в расчете на одного сотрудника {prib_for_one} ')
