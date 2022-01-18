20a1c06b9f679db465c382c0329cf3e36c29050d
jakovp999 <jakovp999@gmail.com>
2022-01-18 11:57:45 +1100

    отформатировал и добавил строчку снизу

--- HW_Lesson_1/les_1_ex_1.py
+++ HW_Lesson_1/les_1_ex_1.py
@@ -5,16 +5,12 @@ print(result_old)
 your_name = input('Как вас завут?        ')
 result_name = f'Привет {your_name} Как у вас дела?    '
 print(result_name)
-your_dela = input(    )
+your_dela = input()
 
 result_all = f'Привет {your_name}, тебе {your_old}, дела у тебя {your_dela}'
 print(result_all)
 
-before_old = int (input (  'лет назад:  '))
+before_old = int(input('лет назад:  '))
 young_old = int(your_old) - before_old
 result_young = f'Тебе было {young_old}'
 print(result_young)
-
-
-
-
