adb7c8e1a6fd0a82974d699f2318c5906be37f90
jakovp999 <jakovp999@gmail.com>
2022-01-18 12:06:19 +1100

    урок 1 задание 3

---
+++ HW_Lesson_1/les_1_ex_3.py
@@ -0,0 +1,10 @@
+number_n = input('Введите число:  ')
+number_nn = number_n * 2
+number_nnn = number_n * 3
+
+# print(number_nn)
+
+result = int(number_nnn) + int(number_nn) + int(number_n)
+
+print(result)
+
