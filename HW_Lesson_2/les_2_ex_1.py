0f5bfd372869a062d408efe626388e5497c9bac9
jakovp999 <jakovp999@gmail.com>
2022-01-24 06:34:52 +1100

    урок 2 задание 2

--- HW_Lesson_2/les_2_ex_1.py
+++ HW_Lesson_2/les_2_ex_1.py
@@ -1,7 +1,7 @@
 spisok = [1,2.2,0o750, None, True, 'hello', ('spam', 'one', 0xaf), {'color':'blue'}]
-print(spisok)
+# print(spisok)
 len_spis = len(spisok)
-print(len_spis)
+# print(len_spis)
 i = 0
 for i in range(len_spis):
     type_chlen = type(spisok[i])
