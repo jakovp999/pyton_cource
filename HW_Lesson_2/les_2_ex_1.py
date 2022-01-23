e08cf12866d1de40dc881cdbe4f4506ee6d576ca
jakovp999 <jakovp999@gmail.com>
2022-01-23 22:16:19 +1100

    урок 2 задание 1

---
+++ HW_Lesson_2/les_2_ex_1.py
@@ -0,0 +1,8 @@
+spisok = [1,2.2,None, True, ('spam', 'one', 0xaf), {'color':'blue'}]
+print(spisok)
+len_spis = len(spisok)
+print(len_spis)
+i = 0
+for i in range(len_spis):
+    type_chlen = type(spisok[i])
+    print (f'{i} член {type_chlen}')
