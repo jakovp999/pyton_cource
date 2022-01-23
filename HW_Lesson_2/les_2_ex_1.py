e07ffed22f3772102f99aa32a5ac8fd9e7be2be5
jakovp999 <jakovp999@gmail.com>
2022-01-23 22:57:03 +1100

    добавил строку

--- HW_Lesson_2/les_2_ex_1.py
+++ HW_Lesson_2/les_2_ex_1.py
@@ -1,4 +1,4 @@
-spisok = [1,2.2,None, True, ('spam', 'one', 0xaf), {'color':'blue'}]
+spisok = [1,2.2,0o750, None, True, 'hello', ('spam', 'one', 0xaf), {'color':'blue'}]
 print(spisok)
 len_spis = len(spisok)
 print(len_spis)
@@ -6,3 +6,4 @@ i = 0
 for i in range(len_spis):
     type_chlen = type(spisok[i])
     print (f'{i} член {type_chlen}')
+
