1dd816e629f0a83b368becddbbb5a6d85b8d51c7
jakovp999 <jakovp999@gmail.com>
2022-01-30 23:24:46 +1100

    улучшил вывод на экран

--- HW_Lesson_4/les_4_ex_2.py
+++ HW_Lesson_4/les_4_ex_2.py
@@ -1,5 +1,6 @@
 list_ish = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 
+print('Исходный ряд', list_ish)
 list_new = [list_ish[i] for i in range(1, len(list_ish)) if list_ish[i] > list_ish[i - 1]]
 
-print(list_new)
+print('Результат - Элементы больше предыдцщего', list_new)
