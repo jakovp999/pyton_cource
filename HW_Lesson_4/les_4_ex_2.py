eb47604638cfcb67563dddfbc790f83757dbe1f6
jakovp999 <jakovp999@gmail.com>
2022-01-30 23:19:35 +1100

    после ctl a;t l

--- HW_Lesson_4/les_4_ex_2.py
+++ HW_Lesson_4/les_4_ex_2.py
@@ -1,5 +1,5 @@
 list_ish = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 
-list_new = [list_ish[i] for i in range (1, len(list_ish)) if list_ish[i] > list_ish[i -1]]
+list_new = [list_ish[i] for i in range(1, len(list_ish)) if list_ish[i] > list_ish[i - 1]]
 
 print(list_new)
