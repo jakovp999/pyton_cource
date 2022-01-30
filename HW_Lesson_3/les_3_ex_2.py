b3ca253d010a1a068df593c707ea653059930621
jakovp999 <jakovp999@gmail.com>
2022-01-30 11:12:28 +1100

    добавил ввод пользователя одного элемента как вариант

--- HW_Lesson_3/les_3_ex_2.py
+++ HW_Lesson_3/les_3_ex_2.py
@@ -2,5 +2,9 @@ def my_dict(**kwargs):
     return kwargs
 
 dict_user = my_dict(Имя='Ганс', фамилия = 'Вайсман', год_рождения = 1950, город_проживания = 'Париж', email = '123@345.com', телефон = '1234567893')
-
 print(dict_user)
+
+n = input('Ведите имя')
+dict_user_1 = my_dict(имя = n )
+print(dict_user_1)
+
