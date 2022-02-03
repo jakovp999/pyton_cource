c61f0ccb734029378ff52231fe719aaa0cd23c17
jakovp999 <jakovp999@gmail.com>
2022-02-03 18:30:16 +1100

    урок 5 задание 2 ctr alt l

--- HW_Lesson_5/les_5_ex_2.py
+++ HW_Lesson_5/les_5_ex_2.py
@@ -7,10 +7,8 @@ my_file = open("new_text.txt", "r")
 print('Колличество строк в файле:', sum(1 for line in my_file))
 my_file.close()
 
-
 my_file = open("new_text.txt", "r")
 data = my_file.read()
 word = data.split()
 print('Колличество слов в фойле:', len(word))
 my_file.close()
-
