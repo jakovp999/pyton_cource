7f4ba0ebdfc8d3e1f00a9f35ccb3dbe4984e2615
jakovp999 <jakovp999@gmail.com>
2022-02-05 10:57:48 +1100

    Добавил второй способ

--- HW_Lesson_5/les_5_ex_4.py
+++ HW_Lesson_5/les_5_ex_4.py
@@ -1,18 +1,28 @@
-with open("file_in_4.txt", "r", encoding='utf - 8') as file_in:
+with open("file_in_4.txt", "r", encoding='utf - 8') as file_in, open("file_out_4.txt", "w") as file_out:
     text = file_in.read()
-
-text = text.replace("One", "Один")
+    text = text.replace("One", "Один")
+    file_out.write(text)
+    text = text.replace("Two", "Два")
+    file_out.write(text)
+    text = text.replace("Three", "Три")
+    file_out.write(text)
+    text = text.replace("Four", "Четыре")
 with open("file_out_4.txt", "w") as file_out:
     file_out.write(text)
 
-text = text.replace("Two", "Два")
-with open("file_out_4.txt", "w") as file_out:
-    file_out.write(text)
 
-text = text.replace("Three", "Три")
-with open("file_out_4.txt", "w") as file_out:
-    file_out.write(text)
 
-text = text.replace("Four", "Четыре")
-with open("file_out_4.txt", "w") as file_out:
-    file_out.write(text)
+#Второй способ
+dict_num = {
+    'One': 'Один',
+    'Two': 'Два',
+    'Three': 'Три',
+    'Four': 'Четыре'
+}
+
+with open("file_in_4.txt", "r", encoding='utf - 8') as file_in, open("file_out_4_1.txt", "w") as file_out:
+    file_lines = file_in.readlines()
+    for line in file_lines:
+        str_spl = line.split()
+        text = dict_num.get(str_spl[0]) #Первое слово в строке англ цифра
+        file_out.write(f'{line.replace(str_spl[0], text)}')
