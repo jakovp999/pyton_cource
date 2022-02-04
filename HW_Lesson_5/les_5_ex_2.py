c8de8c0a3db79669de777b5232ce417d24270b9c
jakovp999 <jakovp999@gmail.com>
2022-02-05 09:22:30 +1100

    Добавил подсчет слов в строке. убрал лишние обращения к файлу

--- HW_Lesson_5/les_5_ex_2.py
+++ HW_Lesson_5/les_5_ex_2.py
@@ -1,14 +1,15 @@
 my_file = open("new_text.txt", "r")
 content = my_file.readlines()
-print('Cодержимое файла:', content)
+data = my_file.read()
 my_file.close()
+print('Cодержимое файла:', content)
 
 my_file = open("new_text.txt", "r")
 print('Колличество строк в файле:', sum(1 for line in my_file))
-my_file.close()
 
-my_file = open("new_text.txt", "r")
-data = my_file.read()
 word = data.split()
 print('Колличество слов в фойле:', len(word))
-my_file.close()
+
+for str, line in enumerate(content):
+    word_kolich = len(line.split())
+    print(f'Колличество слов в строке {str + 1} - {word_kolich}')
